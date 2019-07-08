f5eb9cd79dbdb0dbc5bc41544bd6dcb927227b94
Viraj Patel <Viraj84@gmail.com>
2019-07-08 13:10:44 -0400

    Create working branch navbar

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    Welcome to the Garden Guru
-
-
+      Welcome to the Garden Guru
  
       <p>Search bar</p>
-
-
       
-
     </div>
   );
 }
