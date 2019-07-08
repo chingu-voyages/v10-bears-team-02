a4de52f19df87b7c32ae581e03dff46bbc3c210b
Viraj Patel <Viraj84@gmail.com>
2019-07-08 10:05:18 -0400

    Setup backend server configuration

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+
+//load env variables
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').load();
+}
+
+//set port
+const port = process.env.PORT
+
+//instantiate express
+const app = express()
+
+//use body parser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//start server
+app.listen(port, () => {
+    console.log('server started on: '+ port)
+
+})
