2e2035275b2565daae2b0bb762c169563a4fe761
github username <github email address>
2019-07-10 09:51:20 -0400

    Combine navbar style and component

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -7,20 +7,31 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
-import './NavBar.css'
+const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+  
 
 class NavBar extends Component{
 
     render() { 
-
+        const classes = useStyles()
         return (
-            <div className="root">
+            <div className={classes.root}>
             <AppBar position="static">
               <Toolbar>
-                <IconButton edge="start" className="menuButton" color="inherit" aria-label="Menu">
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
                   <MenuIcon />
                 </IconButton>
-                <Typography variant="h6" className="title">
+                <Typography variant="h6" className={classes.title}>
                   News
                 </Typography>
                 <Button color="inherit">Login</Button>
