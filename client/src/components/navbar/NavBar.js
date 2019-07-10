bac0940f85ac9327b37d3c3b3c4cb9b7a0f2236a
github username <github email address>
2019-07-10 09:20:31 -0400

    Create navbar styles

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react'
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
+import './NavBar.css'
 
 class NavBar extends Component{
-
-
-
 
     render() { 
 
         return (
-            <div>
-
-                Nav bar
-            </div>
+            <div className="root">
+            <AppBar position="static">
+              <Toolbar>
+                <IconButton edge="start" className="menuButton" color="inherit" aria-label="Menu">
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" className="title">
+                  News
+                </Typography>
+                <Button color="inherit">Login</Button>
+              </Toolbar>
+            </AppBar>
+          </div>
 
         )
     }
