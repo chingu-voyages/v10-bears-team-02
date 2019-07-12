30ec4a90dd3dfd7356b3b544fda53528ad8c416e
Ally Kadel <akadel01@gmail.com>
2019-07-12 17:02:03 -0400

    Create react hook for query input change

--- client/src/components/inputField/InputField.js
+++ client/src/components/inputField/InputField.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import useStyles from './InputFieldStyles'
 
 export default function InputField() {
     const classes = useStyles();
 
-    const handleInputChange = event => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    };
+    const [values, setValues] = React.useState({
+        query: ''
+      });
+    
+    //   const handleInputChange = name => event => {
+    //       console.log(event.target.value)
+    //     //   debugger
+    //     setValues({ ...values, [name]: event.target.value });
+    //     console.log()
+    //   };
 
-    <form className={classes.container} noValidate autoComplete="off">
-        <TextField
-            id="standard-with-placeholder"
-            label="With placeholder"
-            placeholder="Placeholder"
-            className={classes.textField}
-            margin="normal"
-            onChange={handleInputChange()}
-        />
-    </form>
+    return (
 
+        <form className={classes.container} noValidate autoComplete="off">
+            <TextField
+                id="standard-with-placeholder"
+                label="With placeholder"
+                placeholder="Placeholder"
+                className={classes.textField}
+                margin="normal"
+                onChange={(event) => {
+                    setValues(event.target.value)
+                    console.log(values)
+                    }
+                }
+            />
+        </form>
+    )
 }
