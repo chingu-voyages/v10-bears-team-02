053f892264be80c0bb0cc396a160930d18bc172a
Ally Kadel <akadel01@gmail.com>
2019-07-12 18:24:58 -0400

    Create search submit handler

--- client/src/components/searchBar/SearchBar.js
+++ client/src/components/searchBar/SearchBar.js
@@ -3,10 +3,17 @@ import InputField from '../inputField/InputField'
 import useStyles from './SearchBarStyles'
 
 function SearchBar() {
+
     const classes = useStyles();
+
+    const handleSearchSubmit = (event) => {
+        event.preventDefault()
+        console.log(event.type)
+    }
     return (
-        <form className={classes.container}>
+        <form className={classes.container} onSubmit={(event) => handleSearchSubmit(event)}>
             <InputField name='query' label='Search for a plant'/>
+            <button>Search</button>
         </form>
     )
 }
