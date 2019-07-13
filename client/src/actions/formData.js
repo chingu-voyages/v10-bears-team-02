268e2bfe0648e0c04ff1f6282056dbc35e92949d
Ally Kadel <akadel01@gmail.com>
2019-07-12 21:59:26 -0400

    Build submitQuery action and connect to SearchBar

--- client/src/actions/formData.js
+++ client/src/actions/formData.js
@@ -7,4 +7,30 @@ export const updateQueryInput = (data) => {
     }
 }
 
+const queryAPI = () => {
+    return {
+        type: 'SUBMIT_QUERY'
+    }
+}
+
+
+
 // ASYNCHRONOUS ACTIONS - make calls to the API
+
+export const submitQuery = (query) => {
+    debugger
+    return dispatch => {
+        dispatch(queryAPI())
+        return fetch(`/api/query`), {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: {
+                query: query
+            }
+        }
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }
+}
