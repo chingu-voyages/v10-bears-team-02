4196073bec7b64311db8885ff1a823e8dd2417c4
Viraj Patel <Viraj84@gmail.com>
2019-07-30 12:26:54 -0400

    Create local authentication signup and login routes

--- server/routes/auth/local/localauth.js
+++ server/routes/auth/local/localauth.js
@@ -13,12 +13,61 @@ function routes(doc, app) {
             if (!user) {
               return done(null, false, { message: 'Incorrect username.' });
             }
+            //use bcrypt here  
             if (!user.validPassword(password)) {
               return done(null, false, { message: 'Incorrect password.' });
             }
             return done(null, user);
           });
         }
-      ));
+    ));
+
+      /**     
+     *  sign up route
+     */        
+    router.route('/api/local_auth/signup')
+        .post((req, res) => { //insert new user form data into db check if username exists, brcypt pw
+            //check if user is in database
+            doc.findOne({ 'user.username': req.body.username }, (err, user) => {
+                //database error
+                if (err) return res.send({ errors: [{ msg: err }] })                
+                //user already exists
+                if(user) return res.send({errors: [{msg:"User already exists try logging in"}]})      
+                
+                //salt and hash password
+                // bcrypt.hash(req.body.password, 10, function(hasherr, hash){                    
+                //     if(hasherr) return res.send({errors: [{ msg: 'hashing error'}]})
+
+                    
+                // })  
+                
+
+                //should salt and hash password above
+                doc.create({                                                             
+                    userId: req.body.username, // unique username
+                    userName: req.body.username,
+                    password: hash                                          
+                },(docerr, result)=>{
+                    if(docerr)return res.send({errors:[{msg:docerr}]})
+                    return res.send({message: "Sign up successfull! Try logging in with your new username and password"})
+                    
+                }) 
+
+            })                            
+        }
+
+    )
+    
+    //authenticate user credentials route
+    router.route('/api/local_auth/login')
+        .post(passport.authenticate('local'), function(req, res){
+            console.log('passed local strategy')
+            if(req.user){
+                console.log('user is:', req.user)
+                return res.send(true)
+            }
+            return res.send(false)
+        }
+    )  
 
 }
