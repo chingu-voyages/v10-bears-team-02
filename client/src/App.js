a1f94c8dbc33d78c2ad37e223efd48c6664a6fa2
Viraj Patel <Viraj84@gmail.com>
2019-07-30 12:26:26 -0400

    Create local authentication signup and login routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,8 @@ import NavBar from './components/navbar/NavBar'
 import SearchBar from './components/searchBar/SearchBar'
 import './App.css';
 import QueriedPlants from './components/queriedPlants/QueriedPlants';
-import PlantCard from './components/plantCard/PlantCard';
 import SinglePlant from './components/singlePlant/SinglePlant'
 import PlantsLibrary from './components/plantsLibrary/PlantsLibrary';
-import SinglePlant from './components/singlePlant/SinglePlant'
 import { Route } from "react-router-dom";
 
 
