38374b46004bf9a56c7ccadfd6c748625cebf525
Viraj Patel <Viraj84@gmail.com>
2019-07-31 22:00:57 -0400

    Move styles to LoginStyles.js

--- client/src/components/loginForm/Login.js
+++ client/src/components/loginForm/Login.js
@@ -9,34 +9,11 @@ import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import useStyles from './LoginStyles';
 import Container from '@material-ui/core/Container';
 
 
-const useStyles = makeStyles(theme => ({
-  '@global': {
-    body: {
-      backgroundColor: theme.palette.common.white,
-    },
-  },
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+
 
 export default function SignIn() {
   const classes = useStyles();
