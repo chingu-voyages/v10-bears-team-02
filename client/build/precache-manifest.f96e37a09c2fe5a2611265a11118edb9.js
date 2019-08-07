cb2fc947cab7cbe5a8b6bf5802a94382474476d7
Viraj Patel <Viraj84@gmail.com>
2019-08-07 15:01:11 -0400

    Create build locally

---
+++ client/build/precache-manifest.f96e37a09c2fe5a2611265a11118edb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2bdd66062e745e6a053ae7997f820c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22a87685e80d02cf402c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "deb6a6db470c48727841",
+    "url": "/static/js/2.e39276de.chunk.js"
+  },
+  {
+    "revision": "22a87685e80d02cf402c",
+    "url": "/static/js/main.6207bf69.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
