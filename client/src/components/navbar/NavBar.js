454bcf7a7ebb185af75e1fdf6708822e4a2c21d4
Viraj Patel <Viraj84@gmail.com>
2019-08-11 23:07:53 -0400

    clean up and route changes

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import useStyles from './NavBarStyles'
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import { connect } from 'react-redux';
 import SearchBar from '../searchBar/SearchBar';
 import  {submitLogOut} from '../../actions/logout'
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+
 
 
 function NavBar(props) {
@@ -51,7 +40,7 @@ function NavBar(props) {
             </Typography>  
           </IconButton>
           
-          <SearchBar {...props} />
+          <SearchBar {...props} className={classes.title}/>
           {props.auth && <Button color="inherit" onClick={handleLogOut}>Log Out</Button>}
 
           <Button
