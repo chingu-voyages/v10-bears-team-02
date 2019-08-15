c83e311eb8a76111264626f95c2c73361e3c78e1
Viraj Patel <Viraj84@gmail.com>
2019-08-14 20:10:31 -0400

    Create dispatch for login errors and update store.js

--- client/src/actions/login.js
+++ client/src/actions/login.js
@@ -15,20 +15,30 @@ const loginSuccess = (payload) => {
     }
 }
 
-// const loginFailed = () => { 
-//     return {
-//         type: 'LOGIN_FAIL'
-//     }
-// }
+const loginFailed = (payload) => { 
+    return {
+        type: 'LOGIN_FAIL',
+        payload
+    }
+}
 
 //Action
 export const submitLogin = (userCreds, history) => {    
     return (dispatch) => {        
        dispatch(submitLoginCredentials()) 
         return axios.post('/api/local_auth/login', userCreds )        
-            .then(response => {       
-                dispatch(loginSuccess(response.data.userData))     
-                history.push('/mygarden')
+            .then(response => {  
+                
+                if (response.data.error) {
+                    //send errors to client                           
+                    dispatch(loginFailed(response.data.error)) // login reducer
+                } 
+                if (response.data.userData) {                                      
+                    dispatch(loginSuccess(response.data.userData)) //userData reducer
+                    history.push('/mygarden')  
+                }
+
+                
             }
         )
     }
